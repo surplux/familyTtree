b2e147906eb1f79cadd33a808862e08830c7ec23
surplux <55955838+surplux@users.noreply.github.com>
2025-10-25 02:37:15 +0200

    Create check-env.js

---
+++ api/check-env.js
@@ -0,0 +1,11 @@
+export const runtime = 'edge';
+
+export async function GET() {
+  const hasAdmin = !!process.env.ADMIN_KEY;
+  const hasBlobToken = !!process.env.BLOB_READ_WRITE_TOKEN; // present when Blob store is connected
+  return new Response(JSON.stringify({
+    ok: true,
+    adminKeyPresent: hasAdmin,
+    blobTokenPresent: hasBlobToken
+  }), { headers: { 'Content-Type': 'application/json' } });
+}
