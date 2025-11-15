e31fd9fed4ec4edc062e9d835ebb83e4754f24e8
surplux <55955838+surplux@users.noreply.github.com>
2025-11-15 16:20:51 +0100

    Update check-env.js

--- api/check-env.js
+++ api/check-env.js
@@ -1,9 +1,11 @@
-// check-env.js
 export const runtime = 'edge';
 
 export async function GET() {
   const hasAdmin = !!process.env.ADMIN_KEY;
-  const hasBlobToken = !!process.env.BLOB_READ_WRITE_TOKEN; // present when Blob store is connected
+  const hasBlobToken =
+    !!process.env.BLOB_READ_WRITE_TOKEN ||
+    !!process.env.ttreeblob_READ_WRITE_TOKEN;
+
   return new Response(JSON.stringify({
     ok: true,
     adminKeyPresent: hasAdmin,
