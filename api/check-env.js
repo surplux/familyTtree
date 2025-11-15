468fc338601dc42a593ad7109781d9c701ae0002
surplux <55955838+surplux@users.noreply.github.com>
2025-11-15 16:36:23 +0100

    Update check-env.js

--- api/check-env.js
+++ api/check-env.js
@@ -3,7 +3,8 @@ export const runtime = 'edge';
 export async function GET() {
   const hasAdmin = !!process.env.ADMIN_KEY;
   const hasBlobToken =
-    !!process.env.BLOB_READ_WRITE_TOKEN;
+    !!process.env.BLOB_READ_WRITE_TOKEN ||
+    !!process.env.ttreeblob_READ_WRITE_TOKEN;
 
   return new Response(JSON.stringify({
     ok: true,
